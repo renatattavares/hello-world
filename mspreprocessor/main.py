dcee61ff95e9d34ad0ef35ee333e60bce0b00956
Renata Tavares <renatattavares@hotmail.com>
2019-02-07 00:55:31 -0300

    Iniciada função do upscaling

--- mspreprocessor/main.py
+++ mspreprocessor/main.py
@@ -8,52 +8,33 @@ from math import pi, sqrt
 from pymoab import rng, types
 from meshHandle.multiscaleMesh import FineScaleMeshMS as msh
 from meshHandle.corePymoab import CoreMoab as core
+from tpfa.assembly import Assembly
+from tpfa.boundary_conditions import BoundaryConditions
 from scipy.sparse import csr_matrix, lil_matrix
 from scipy.sparse.linalg import spsolve
 
-################################################################################
 def equiv_perm(k1, k2):
     return (2*k1*k2)/(k1 + k2)
 
 def centroid_dist(c1, c2):
     return ((c1-c2)**2).sum()
 
-def pressao_prescrita(coef, num_elements, nx, ny):
-    q = lil_matrix((num_elements, 1), dtype=np.float_)
-    coef[0:nx*ny] = 0
-    q [0:nx*ny] = 500
-    coef[num_elements-(nx*ny):num_elements] = 0
-    for r in range(nx*ny):
-        coef[r,r] = 1
-        coef[r+num_elements-(nx*ny),r+num_elements-(nx*ny)] = 1
-    return coef, q
-
-################################################################################
-dx, dy, dz = 1, 1, 1
-
 print("Initializating mesh")
+start = time.time()
+dx, dy, dz = 1, 1, 1
+nx, ny, nz = 10, 10, 10
+num_elements = nx*ny*nz
 M = msh("malha-teste.h5m", dim = 3)
 vec = np.arange(len(M.alma)).astype(int)
+end = time.time()
+print("This step lasted {0}s".format(end-start))
 
-#################### Informações de condições de contorno ######################
-num_bc = 1 # 1 para pressão prescrita, 2 para vazão, 3 para mista
-value = 500
-linha = 0
-coluna = 0
-nx = 10
-ny = 10
-nz = 10
-num_elements = nx*ny*nz
-
-
-################################################################################
-print("Setting the variables")
+print("Setting the permeability")
 M.permeability[:] = 1
 
-print("Assembly")# Montagem da matriz de coeficientes do sistema.
+print("Assembly")
 start = time.time()
 coef = lil_matrix((num_elements, num_elements), dtype=np.float_)
-
 for i in range(num_elements):
     adjacencies = M.volumes.bridge_adjacencies(i, 2, 3)
     length = np.shape(adjacencies)
@@ -65,7 +46,10 @@ end = time.time()
 print("This step lasted {0}s".format(end-start))
 
 print("Setting boundary conditions")
-coef, q = pressao_prescrita(coef, num_elements, nx, ny)
+start = time.time()
+bc = BoundaryConditions(num_elements, nx, ny, coef)
+end = time.time()
+print("This step lasted {0}s".format(end-start))
 
 '''
 workbook = xlsxwriter.Workbook('coef_mspreprocessor.xlsx')
@@ -83,14 +67,38 @@ workbook.close()
 '''
 
 print("Solving the problem")
-coef = lil_matrix.tocsr(coef)
-q = lil_matrix.tocsr(q)
+start = time.time()
+coef = lil_matrix.tocsr(bc.coef)
+q = lil_matrix.tocsr(bc.q)
 P = spsolve(coef,q)
+end = time.time()
+print("This step lasted {0}s".format(end-start))
 
 print("Storing results")
+start = time.time()
 for i in range(num_elements):
     M.pressure[i] = P[i]
     #M.erro[i] = P[i]-P_analitico[i]
+end = time.time()
+print("This step lasted {0}s".format(end-start))
+
+area = dx*dy*25
+coef = lil_matrix((50, 50), dtype=np.float_)
+#for a in range(len(M.coarse_volumes)):
+for b in range(25):
+    adjacencies = M.coarse_volumes[0].volumes.bridge_adjacencies(b, 2, 3)
+    length = np.shape(adjacencies)
+    for c in range(length[1]):
+        id = np.array([adjacencies[0][c]],  dtype= np.int)
+        coef[b,id] = equiv_perm(M.permeability[M.coarse_volumes[0].volumes.global_id[b]], M.permeability[M.coarse_volumes[0].volumes.global_id[id]])/centroid_dist(M.volumes.center[M.coarse_volumes[0].volumes.global_id[b]], M.volumes.center[M.coarse_volumes[0].volumes.global_id[id]])
+    coef[b,b] = (-1)*coef[b].sum()
+
+coef = lil_matrix.tocsr(bc.coef)
+dp = lil_matrix((1,50), dtype=np.float_)
+dp [:] = 500
+dp = lil_matrix.tocsr(dp)
+
+q = np.dot(dp,coef)
 
 print("Printing results")
 M.core.print()
